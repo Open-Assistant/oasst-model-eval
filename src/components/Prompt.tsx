fbd5ce3ca9c154b2081d0060cd51b9739c6ebf58
John Tapsell <johnflux@gmail.com>
2023-03-02 21:48:28 -0800

    Add different colors to the different models

--- src/components/Prompt.tsx
+++ src/components/Prompt.tsx
@@ -1,4 +1,5 @@
 import { PromptResults } from "../FileComparer";
+import './Prompt.css';
 
 interface PromptProps {
   prompt: string;
@@ -11,20 +12,20 @@ export const Prompt = ({prompt, results, outputIndex, showSamplingMethod}: Promp
   return (
     <div>
       <div className="promptBubble">{prompt}</div>
-      <table>
+      <table className={showSamplingMethod ? 'replyTableWithAltRows' : ''}>
         <tbody>
-          {results.map(result => <tr key={result.file.model_name}>
+          {results.map((result, modelIndex) => <tr key={result.file.model_name}>
             <td className="model_name">{result.file.model_name}</td>
             <td>{result.results.map((result, result_index) => (
               <div key={result_index}>
-              <div className="sampling_config">
-                {showSamplingMethod && <div>Sampling config: {result.sampling_config}</div>}
-                {Object.keys(result.sampling_params).map(param => <div key={param}>{param}: {result.sampling_params[param].toString()}</div>)}
-              </div>
-              {outputIndex === -1 ?
-                result.outputs.map((output, index) => <div key={index} className="replyBubble">{output}</div>) :
-                <div className="replyBubble">{result.outputs[outputIndex]}</div>
-              }
+                <div className="sampling_config">
+                  {showSamplingMethod && <div>Sampling config: <b>{result.sampling_config}</b></div>}
+                  {Object.keys(result.sampling_params).map(param => <div key={param}>{param}: <span className="param-value">{result.sampling_params[param].toString()}</span></div>)}
+                </div>
+                {outputIndex === -1 ?
+                  result.outputs.map((output, index) => <ReplyBubble key={index} modelIndex={modelIndex} output={output} saturation={result_index}/>) :
+                  <ReplyBubble modelIndex={modelIndex} output={result.outputs[outputIndex]} saturation={result_index} />
+                }
               </div>
             ))}</td>
           </tr>)}
@@ -33,3 +34,8 @@ export const Prompt = ({prompt, results, outputIndex, showSamplingMethod}: Promp
     </div>
   );
 }
+
+const ReplyBubble = ({modelIndex, output, saturation} : { modelIndex: number, output: string, saturation: number }) => {
+  return <div className="replyBubble" style={{background: `hsl(${194 + 42.3 * modelIndex} ${30 + 23 / (saturation + 1)}% ${73 + 10 / (saturation+1)}%)`}}>{output}</div>
+}
+
