24a23a3372cc6005478fc462a9dd6f00d878d1a7
John Tapsell <johnflux@gmail.com>
2023-03-03 14:41:35 -0800

    Add expanded/collapses bubble, and option to hide params

--- model_comparer/src/components/Prompt.tsx
+++ model_comparer/src/components/Prompt.tsx
@@ -6,31 +6,36 @@ interface PromptProps {
   results: PromptResults[];
   showSamplingMethod: boolean;
   outputIndex: number;
+  showSamplingConfig: boolean;
+  collapsed?: boolean;
+  onToggleCollapsed?: () => void;
 };
 
-export const Prompt = ({prompt, results, outputIndex, showSamplingMethod}: PromptProps) => {
+export const Prompt = ({collapsed, onToggleCollapsed, prompt, results, outputIndex, showSamplingMethod, showSamplingConfig}: PromptProps) => {
   return (
     <div>
-      <div className="promptBubble">{prompt}</div>
-      <table className={showSamplingMethod ? 'replyTableWithAltRows' : ''}>
-        <tbody>
-          {results.map((result, modelIndex) => <tr key={result.file.model_name}>
-            <td className="model_name">{result.file.model_name}</td>
-            <td>{result.results.map((result, result_index) => (
-              <div key={result_index}>
-                <div className="sampling_config">
-                  {showSamplingMethod && <div>Sampling config: <b>{result.sampling_config}</b></div>}
-                  {Object.keys(result.sampling_params).map(param => <div key={param}>{param}: <span className="param-value">{result.sampling_params[param].toString()}</span></div>)}
+      <div className="promptBubble" onClick={onToggleCollapsed}>{prompt}</div>
+      { !collapsed &&
+        <table className={showSamplingMethod ? 'replyTableWithAltRows' : ''}>
+          <tbody>
+            {results.map((result, modelIndex) => <tr key={result.file.model_name}>
+              <td className="model_name">{result.file.model_name}</td>
+              <td>{result.results.map((result, result_index) => (
+                <div key={result_index}>
+                  {showSamplingConfig && <div className="sampling_config">
+                    {showSamplingMethod && <div>Sampling config: <b>{result.sampling_config}</b></div>}
+                    {Object.keys(result.sampling_params).map(param => <div key={param}>{param}: <span className="param-value">{result.sampling_params[param].toString()}</span></div>)}
+                  </div>}
+                  {outputIndex === -1 ?
+                    result.outputs.map((output, index) => <ReplyBubble key={index} modelIndex={modelIndex} output={output} saturation={result_index}/>) :
+                    <ReplyBubble modelIndex={modelIndex} output={result.outputs[outputIndex]} saturation={result_index} />
+                  }
                 </div>
-                {outputIndex === -1 ?
-                  result.outputs.map((output, index) => <ReplyBubble key={index} modelIndex={modelIndex} output={output} saturation={result_index}/>) :
-                  <ReplyBubble modelIndex={modelIndex} output={result.outputs[outputIndex]} saturation={result_index} />
-                }
-              </div>
-            ))}</td>
-          </tr>)}
-        </tbody>
-      </table>
+              ))}</td>
+            </tr>)}
+          </tbody>
+        </table>
+      }
     </div>
   );
 }
