9a2ed9760cdf119e0ce3dc32d5313cc0af50068e
Shahules786 <shahules786@gmail.com>
2023-03-16 17:56:27 +0000

    black formating

--- model_eval/scoring/score.py
+++ model_eval/scoring/score.py
@@ -7,51 +7,60 @@ import torch
 import numpy as np
 import pandas as pd
 
+
 def load_sampling_data(path):
+    """
+    Load sampling data and ensure appropriate keys are present.
+    """
 
     if os.path.exists(path):
         data = json.load(open(path))
     else:
         raise FileNotFoundError(f"Sampling data {path} not found")
-  
+
     if "prompts" not in data.keys():
         raise KeyError("sampling data should contain prompts key")
 
     keys = set(data["prompts"][0].keys())
-    required_keys = set(["prompt","results"])
+    required_keys = set(["prompt", "results"])
     keys = keys.intersection(required_keys)
     if keys != required_keys:
         raise KeyError(f"Missing keys {required_keys - keys} ")
-    
-    return data
-    
-def batch_inference(model, dataloader):
 
-    scores, sampling = [],[]
+    return data
+
+
+def batch_inference(model, dataloader):
+    """
+    Batch inference
+    """
+
+    scores, sampling = [], []
     for i, data in enumerate(dataloader):
         sampling.append(data.pop("sampling").cpu().detach().numpy())
-        data = {k:v.squeeze() for k,v in data.items()}
-        pred = model(**data).logits[:,0].cpu().detach().numpy()
+        data = {k: v.squeeze() for k, v in data.items()}
+        pred = model(**data).logits[:, 0].cpu().detach().numpy()
         scores.append(pred)
 
-    return np.hstack(sampling),np.hstack(scores)
-        
+    return np.hstack(sampling), np.hstack(scores)
 
 
 if __name__ == "__main__":
-    
     parser = argparse.ArgumentParser(description="")
     parser.add_argument("--data_path", type=str, help="Path of the sampling data file")
     parser.add_argument("--model_path", type=str, help="Path or url of the model file")
-    parser.add_argument("--device", type=str, help="device",default="cpu")
-    parser.add_argument("--save", type=bool, help="whether to save the results",default=True)
-
+    parser.add_argument("--device", type=str, help="device", default="cpu")
+    parser.add_argument(
+        "--save", type=bool, help="whether to save the results", default=True
+    )
 
     args = parser.parse_args().__dict__
-    if args.get("device")!="cpu":
-      device = torch.device("cuda") if torch.cuda.is_available() else torch.device('cpu')
+    if args.get("device") != "cpu":
+        device = (
+            torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+        )
     else:
-      device = torch.device('cpu')
+        device = torch.device("cpu")
 
     data = load_sampling_data(args.get("data_path"))
 
@@ -60,26 +69,16 @@ if __name__ == "__main__":
     tokenizer = AutoTokenizer.from_pretrained(reward_name)
     rank_model.eval()
     rank_model.to(device)
-  
 
     dataloader = get_dataloader(data, tokenizer, 512, 4, device)
     sampling, scores = batch_inference(rank_model, dataloader)
-    print(sampling.shape,scores.shape)
-    df = pd.DataFrame({"sampling":sampling,"score":scores})
-    id2label = {v:k for k,v in dataloader.dataset.label2id.items()}
+    print(sampling.shape, scores.shape)
+    df = pd.DataFrame({"sampling": sampling, "score": scores})
+    id2label = {v: k for k, v in dataloader.dataset.label2id.items()}
     df["sampling"] = df["sampling"].map(id2label)
     results = df.groupby("sampling")["score"].mean().to_dict()
     print("RESULTS: ", results)
-    
+
     if args.get("save"):
-        with open("results.json",'w') as file:
-            json.dump(results,file,indent=4)
-
-
-
-
-
-    
-
-
-    
+        with open("results.json", "w") as file:
+            json.dump(results, file, indent=4)
